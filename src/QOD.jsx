6d76c24deb1406dff1aab3b8376ebc217d5783fd
Kgardiner15 <Kaeleigh.gardiner@yahoo.ca>
2024-04-03 18:25:38 -0400

    add multiple questions

--- src/QOD.jsx
+++ src/QOD.jsx
@@ -4,8 +4,99 @@ import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 
 export default function () {
+    const questions = [{ 
+                type: "radiogroup",
+                name: "question1",
+                title: "What is a PFD",
+                choices: [
+                    "Personal Flotation Device", "Portable Flotation Device", "Phone Finder Device", "People Finding Divers"
+                ],
+                correctAnswer: "Personal Flotation Device"
+            }, 
+            {
+                type: "radiogroup",
+                name: "question2",
+                title: "If there are 4 people on a boat how many life jackets should you have aboard?",
+                choices: [
+                    "1", "2", "4", "5"
+                ],
+                correctAnswer: "4"
+            },
+            {
+                type: "radiogroup",
+                name: "question3",
+                title: "True or false: Boat lights are required when traveling at night?",
+                choices: [
+                    "True", "False"
+                ],
+                correctAnswer: "True"
+            },
+            {
+                type: "radiogroup",
+                name: "question4",
+                title: "Which items should be located on a boat at all times?",
+                choices: [
+                    "Lifejacket", "Fire Extinguishers", "Wistle ", "All of the above"
+                ],
+                correctAnswer: "All of the above"
+            },
+            {
+                type: "radiogroup",
+                name: "question5",
+                title: "At what age are you allowed to operate a boat with 40hp?",
+                choices: [
+                    "12", "14", "16", "18"
+                ],
+                correctAnswer: "12"
+            },
+            {
+                type: "radiogroup",
+                name: "question6",
+                title: "True or False: Pets (like dogs and cats) are allowed on boats?",
+                choices: [
+                    "True", "False"
+                ],
+                correctAnswer: "True"
+            },
+            {
+                type: "radiogroup",
+                name: "question7",
+                title: "Do you need a boaters licence to drive a boat?",
+                choices: [
+                    "No", "Yes", "Only need one if its a large boat"
+                ],
+                correctAnswer: "Yes"
+            },
+            {
+                type: "radiogroup",
+                name: "question8",
+                title: "Children under what age are required to wear a life jacket?",
+                choices: [
+                    "5", "8", "12", "They dont need one if they can swim"
+                ],
+                correctAnswer: "12"
+            },
+            {
+                type: "radiogroup",
+                name: "question9",
+                title: "What is the most common cause of boating injuries?",
+                choices: [
+                    "Boat fires", "Wind", "Sunburns", "Falling overboard"
+                ],
+                correctAnswer: "Falling overboard"
+            },
+            {
+                type: "radiogroup",
+                name: "question10",
+                title: "What is the number one contributing factor to boating deaths in Canada?",
+                choices: [
+                    "Capsized boat", "Failure to wear PFD", "Severe weather", "Sharks"
+                ],
+                correctAnswer: "Failure to wear PFD"
+            }];
+            const nQuestion = Math.floor((Math.random() * questions.length));
     const surveyJson = {
-        title: "Lifeguard Safety",
+        title: "Boating Safety",
         showCorrectAnswer: "always",
         showProgressBar: "bottom",
         firstPageIsStarted: true,
@@ -13,7 +104,7 @@ export default function () {
         pages: [{
             elements: [{
                 type: "html",
-                html: "You are about to start a quiz on Lifeguard Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+                html: "You are about to start a quiz on Boating Safety. <br>You will have 30 seconds for every question and 60 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
             }, {
                 type: "text",
                 name: "username",
@@ -21,15 +112,7 @@ export default function () {
                 isRequired: true
             }]
         }, {
-            elements: [{
-                type: "radiogroup",
-                name: "question1",
-                title: "What is the most common cause of lifeguard injuries?",
-                choices: [
-                    "Slip and fall accidents", "Pool chemicals", "Sunburns", "Sharks"
-                ],
-                correctAnswer: "Slip and fall accidents"
-            }]
+            elements: [questions[nQuestion]]
         }]
     };
     const survey = new Model(surveyJson);
